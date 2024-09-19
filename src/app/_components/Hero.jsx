0056d8fc3e9353cc76df2e7fd49a1322efdd1d1e
sadafqureshia <154066331+sadafqureshia@users.noreply.github.com>
2024-09-19 11:21:54 +0530

    Update Hero.jsx

--- src/app/_components/Hero.jsx
+++ src/app/_components/Hero.jsx
@@ -10,7 +10,7 @@ function Hero() {
           titleComponent={
             <>
               <h1 className="text-4xl font-semibold text-black dark:text-white">
-                Manage your Money with AI-Driven Personal <br />
+                Manage your Money with Personal Expense Tracker <br />
                 <span className="text-4xl md:text-[6rem] text-blue-800 font-bold mt-1 leading-none">
                   FinFlow
                 </span>
